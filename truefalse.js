4c21ac10493b8725dcf188df2720d7e912aa427c
Raihanuddin-anik <raihanuddinanikm@gmail.com>
2020-08-12 22:17:57 +0600

    add

---
+++ truefalse.js
@@ -0,0 +1,43 @@
+// const age = 0;
+
+// if(age){
+//     console.log("condition is true");
+// // if varrible is a number javascript will show true. all number is true without zero.
+// // or if varrible is zero javascript will show false
+
+
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+
+
+// const name = "";
+
+// if(name){
+//     console.log("condition is true");
+// // if varrible is carrecter javascript will show true. all string is true without empty string.
+
+// // or if varrible is empty javascript will show false
+
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+
+let name = NaN;
+console.log(name)
+
+if(name){
+    console.log("condition is true");
+// if varrible is carrecter javascript will show true. all string is true without empty string.
+
+// or if varrible is empty javascript will show false
+
+}
+else{
+    console.log("condition is false")
+}
+
